a724958ae40f161c62bc7ffb3f9160ad45954968
Pavel Volkov <pavel@mail.ru>
2017-04-11 18:44:33 +0300

    make tournament

--- gulp/js/admin.js
+++ gulp/js/admin.js
@@ -40,3 +40,123 @@ $(function(){
 
 	})
 })
+
+$(document).ready(function() {
+	var val = $("input:checked").val();
+	if(val == 1) {
+		$(".schema1").css("display", "block");
+		$(".schema2").css("display", "block");
+		$(".schema3").css("display", "none");
+	} else if (val == 2) {
+		$(".schema1").css("display", "block");
+		$(".schema2").css("display", "none");
+		$(".schema3").css("display", "none");
+	} else {
+		$(".schema1").css("display", "block");
+		$(".schema2").css("display", "block");
+		$(".schema3").css("display", "block");
+	}
+})
+
+$(function() {
+	$(document).on("focus", ".radios", function() {
+		var val = $(this).val();
+		console.log(val);
+	if(val == 1) {
+		$(".schema1").show(500);
+		$(".schema2").show(500);
+		$(".schema3").hide(500);
+	} else if (val == 2) {
+		$(".schema1").show(500);
+		$(".schema2").hide(500);
+		$(".schema3").hide(500);
+	} else {
+		$(".schema1").show(500);
+		$(".schema2").show(500);
+		$(".schema3").show(500);
+	}
+	})
+})
+
+// change users //
+$(document).ready(function() {
+	var howChanges = $('.tournament-user div').length;
+	$('.allow').html(howChanges);
+})
+
+// add users for tournament //
+$(function() {
+	$(document).on('click', '.preteam', function() {
+		var element = $(this);
+		var rowid = element.attr("id");
+		var id = rowid.substr(1);
+		element.removeClass("preteam");
+		element.addClass("team");
+
+		var schema = $('#schema').html();
+		var how = $('.tournament-user div').length;
+
+		if(schema == 1) {
+			var users = $(".tournament-user");
+			users.append(element);
+		} else if (schema == 2) {
+			var users = $(".tournament-user");
+			users.append(element);
+
+			if((how % 2) == 0)
+				users.append(" - ");
+			else
+				users.append("<br>");
+
+		} else if (schema == 3) {
+			var usergroups = $("#groups").html();
+			var users = $(".tournament-user");
+			
+			if(how % usergroups == 0) {
+				var group;
+				if(how == 0)
+					group = "A";
+				if(how == 4)
+					group = "B";
+				if(how == 8)
+					group = "C";
+				if(how == 12)
+					group = "D";
+				if(how == 16)
+					group = "E";
+				if(how == 20)
+					group = "F";
+				if(how == 24)
+					group = "G";
+				if(how == 26)
+					group = "H";
+				users.append("<br>ГРУППА " + group + "<br>");
+			}
+			
+			users.append(element);
+
+		}
+
+		var input = $('.form-hidden');
+		input.append('<input type="hidden" id="form_user_' + id + '" name="form[user][' + id + ']" value="' + id + '" class="form-control" />');
+
+		var endhow = $('.tournament-user div').length;
+		$('.allow').html(endhow);
+		var all = $('.all').html();
+		var button = $('.but');
+		if($('#groups').html() == 0) {
+			if(all == endhow) {
+				console.log('all ' + all + ' allow ' + endhow);
+				button.show();
+			}
+			else {
+				button.hide();
+			}
+		} else {
+			if(endhow == 2 || endhow == 4 || endhow == 8 || endhow == 16 || endhow == 32 || endhow == 64 || endhow == 128 || endhow == 256)
+				button.show();
+			else
+				button.hide();			
+		}
+	});
+})
