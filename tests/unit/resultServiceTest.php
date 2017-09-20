d05c250fb05889ed0e8ae0368f2b62e5410e2fd6
Pavel Volkov <pavel@mail.ru>
2017-09-20 12:46:35 +0300

    make processing and annihilation test

--- tests/unit/resultServiceTest.php
+++ tests/unit/resultServiceTest.php
@@ -2,7 +2,7 @@
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class resultServiceTest extends KernelTestCase
+class resultsServiceTest extends KernelTestCase
 {
 	public function setUp()
 	{
@@ -12,6 +12,7 @@ class resultServiceTest extends KernelTestCase
 
 	public function testCalculate()
 	{
+		// processing test
 		$result = $this->results_tournament->processing([1, 2, 3], [1, 2, 3]);
 		$this->assertEquals(3, $result);
 
@@ -22,6 +23,24 @@ class resultServiceTest extends KernelTestCase
 		$this->assertEquals(1, $result);
 
 		$result = $this->results_tournament->processing([2, 2, 2], [1, 2, 3]);
-		$this->assertEquals(0, $result);		
+		$this->assertEquals(0, $result);
+
+		// annihilation test
+		$class = new ReflectionClass($this->results_tournament);
+		$method = $class->getMethod('annihilation');
+		$method->setAccessible(true);
+		$obj = $this->results_tournament;
+
+		$result = $method->invoke($obj, [1, 0]);
+		$this->assertEquals([1, 1, 1, 1], $result);
+
+		$result = $method->invoke($obj, [0, 0]);
+		$this->assertEquals([0, 0, 0, 0], $result);
+
+		$result = $method->invoke($obj, [3, 1]);
+		$this->assertEquals([1, 2, 3, 4], $result);
+
+		$result = $method->invoke($obj, [1, 2]);
+		$this->assertEquals([2, 1, 1, 3], $result);						
 	}
 }
